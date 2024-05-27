7bf10709acc8d801eac83a879247b13aa850566c
lostqubit <sg_suryaprakash@yahoo.com>
2024-05-27 14:07:59 +0530

    generate random starting config

--- src/models.js
+++ src/models.js
@@ -32,13 +32,37 @@ const Gameboard = () => {
 		board.push(row);
 	}
 
-	const canPlaceShip = (ship, xStart, yStart, orientation) => {
-		if (orientation === 0 && xStart + ship.length > 9) return false;
-		if (orientation === 1 && yStart + ship.length > 9) return false;
+	const init = () => {
+		for(let i=0;i<10;i++){
+			for(let j=0;j<10;j++){
+				board[i][j] = 0;
+			}
+		}
+	};
+
+	const convertCoordinates = (cell) => {
+		return [Math.floor(cell / 10), cell % 10];
+	};
+
+	const canPlaceShip = (ship, cell, orientation) => {
+		const [xStart, yStart] = convertCoordinates(cell);
+		if (orientation === 0){
+			if(xStart + ship.length >9) return false;
+			for(let i=xStart;i<xStart+ship.length;i++){
+				if(board[i][yStart]!==0) return false;
+			}
+		}
+		if (orientation === 1){
+			if(yStart + ship.length > 9) return false;
+			for (let i = yStart; i < yStart + ship.length; i++){
+				if(board[xStart][i]!==0) return false;		
+			}
+		}
 		return true;
 	};
 
-	const placeShip = (ship, xStart, yStart, orientation) => {
+	const placeShip = (ship, cell, orientation) => {
+		const [xStart, yStart] = convertCoordinates(cell);
 		if (orientation === 0) {
 			for (let i = xStart; i < xStart + ship.length; i++)
 				board[i][yStart] = ship;
@@ -48,7 +72,8 @@ const Gameboard = () => {
 		}
 	};
 
-	const processAttack = (x, y) => {
+	const processAttack = (cell) => {
+		const [x, y] = convertCoordinates(cell);
 		if (board[x][y] === 0) board[x][y] = -1;
 		else if (board[x][y] !== 1) {
 			const ship = board[x][y];
@@ -63,7 +88,34 @@ const Gameboard = () => {
 		return false;
 	};
 
-	return { canPlaceShip, placeShip, processAttack, checkGameOver };
+	const generateRandomCoords = () => {
+		return [Math.floor(Math.random()*100), Math.floor(Math.random()*2)];
+	}
+
+	const placeShipRandom = (ship) => {
+		const [cell, orientation] = generateRandomCoords();
+		if(canPlaceShip(ship,cell,orientation)){
+			placeShip(ship,cell,orientation);
+			return null;
+		}
+		else{
+			placeShipRandom(ship);
+		}
+	};
+
+	const generateRandomConfig = () => {
+		init();
+		const ships = [Ship(5),Ship(4),Ship(3),Ship(3),Ship(2)];
+		
+
+		for(let ship of ships){
+			placeShipRandom(ship);
+		}
+
+		console.log(board);
+	};
+
+	return { canPlaceShip, placeShip, processAttack, checkGameOver, generateRandomConfig };
 };
 
 const Player = (name) => {
