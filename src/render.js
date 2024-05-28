e81945122443c79547bf33f5d4251c50c971d8a2
lostqubit <sg_suryaprakash@yahoo.com>
2024-05-28 09:10:54 +0530

    display winner of round

--- src/render.js
+++ src/render.js
@@ -1,34 +1,46 @@
 const display = (() => {
+	const root = document.querySelector("#root");
 	const container = document.querySelector("#root>div");
 
-    const reset = () => {
-        container.innerHTML = "";
-    };
-	
-    const generateBoard = (id) => {
-        const wrapper = document.createElement("div");
+	const reset = () => {
+		container.innerHTML = "";
+	};
+
+	const winnerScreen = (winner) => {
+		const div = document.createElement("div");
+		div.classList.add("winner");
+		const h1 = document.createElement("h1");
+		if (winner === 1)
+			h1.innerText = "Enemy fleet has been destroyed! You win!";
+		else h1.innerText = "Your fleet has been destroyed! You Lose!";
+		div.appendChild(h1);
+		root.after(div);
+	};
+
+	const generateBoard = (id) => {
+		const wrapper = document.createElement("div");
 		const grid = document.createElement("div");
 		grid.classList.add("grid");
 		for (let i = 0; i < 100; i++) {
 			const cell = document.createElement("div");
-			cell.id = `cell${id}-${i}`;
-            cell.classList.add("cell");
-            grid.appendChild(cell);
-
-            cell.addEventListener("click", (event) => {
-                const board = event.target.id[4];
-                const cellId = event.target.id.split("-")[1];
-            });
+			cell.classList.add("cell");
+			if (id === 2) cell.id = `cell-${i}`;
+			grid.appendChild(cell);
 		}
-        const heading = document.createElement("h2");
-        if(id===1) heading.innerText = "Friendly Waters";
-        else heading.innerText = "Enemy Waters";
-        wrapper.appendChild(heading);
-        wrapper.appendChild(grid);
-        container.appendChild(wrapper);
+		const heading = document.createElement("h2");
+		if (id === 1) {
+			grid.id = "player";
+			heading.innerText = "Friendly Waters";
+		} else {
+			grid.id = "computer";
+			heading.innerText = "Enemy Waters";
+		}
+		wrapper.appendChild(heading);
+		wrapper.appendChild(grid);
+		container.appendChild(wrapper);
 	};
 
-    return {generateBoard};
+	return { reset, generateBoard, winnerScreen };
 })();
 
 export default display;
